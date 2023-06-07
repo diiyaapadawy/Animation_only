db889729898e8f6ae67686774d01c3358007981e
Diaa Ashraf <129322441+diiyaapadawy@users.noreply.github.com>
2023-06-07 04:40:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+(()=>{
+    const openNavMenu = document.querySelector(".open-nav-menu"),
+    closeNavMenu = document.querySelector(".close-nav-menu"),
+    navMenu = document.querySelector(".nav-menu");
+    menuOverlay = document.querySelector(".menu-overlay"),
+    mediaSize = 991;
+
+
+    openNavMenu.addEventListener("click", toggleNav);
+    closeNavMenu.addEventListener("click", toggleNav);
+    //Close the navMenu by Clicking Outside.
+    menuOverlay.addEventListener("click", toggleNav);
+
+    function toggleNav(){
+        navMenu.classList.toggle("open");
+        menuOverlay.classList.toggle("active");
+        document.body.classList.toggle("hidden-scrolling");
+    }
+    navMenu.addEventListener("click", (event)=>{
+        if(event.target.hasAttribute("data-toggle") &&
+        window.innerWidth <= mediaSize){
+            const menuItemHasChildren = event.target.parentElement;
+            //if menuItemHasChildren is already expanded, collapse it
+            if(menuItemHasChildren.classList.contains("active")){
+                collapseSubMenu();
+            }else{
+                //Collaps Existing Expanded menuItemHasChildren
+                if(navMenu.querySelector('.menu-item-has-children.active')){
+                    collapseSubMenu();
+                }
+                //expanded new menuItemHasChildren
+                menuItemHasChildren.classList.add("active");
+                const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            }
+        }
+    });
+    function collapseSubMenu(){
+        navMenu.querySelector('.menu-item-has-children.active .sub-menu')
+        .removeAttribute("style");
+        navMenu.querySelector('.menu-item-has-children.active')
+        .classList.remove("active");
+    }
+    function resizeFix(){
+        // if navMenu is open , close it
+        if(navMenu.classList.contains("open")){
+            toggleNav();
+        }
+        // if menu-item-has-children is expanded , collapse it
+        if(navMenu.querySelector('.menu-item-has-children.active')){
+            collapseSubMenu();
+        }
+    }
+    window.addEventListener("resize", function(){
+        if(this.innerWidth > mediaSize){
+            this.resizeFix()
+        }
+    })
+})();
